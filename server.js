2323f50ccd2281bc366e8429cc700230b01b21d6
Ansh <anshparikh.1305@gmail.com>
2025-06-10 15:07:02 +0530

    b59

--- server.js
+++ server.js
@@ -11,17 +11,11 @@ dotenv.config();
 // Create Express app
 const app = express();
 
-// CORS configuration
-const corsOptions = {
-  origin: ['http://localhost:5173', 'https://gatistwam.com', 'https://gatistwam.netlify.app'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-  maxAge: 86400 // 24 hours
-};
-
 // Middleware
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: ['https://gatistwam.netlify.app', 'http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
